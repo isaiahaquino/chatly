941eb7f8be0ce6ee50c965fb83d4bf5a160ad447
isaiahaquino <isaiahaquino1997@gmail.com>
2024-05-08 13:04:48 -0700

    initial backend

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,11 @@
 import express from "express";
 import dotenv from "dotenv";
+import cookieParser from "cookie-parser";
 
 import authRoutes from "../backend/routes/authRoutes.js";
+import messageRoutes from "../backend/routes/messageRoutes.js";
+import userRoutes from "../backend/routes/userRoutes.js";
+
 import connectToMongoDB from "./db/connectToMongoDB.js";
 
 const app = express();
@@ -10,8 +14,11 @@ const PORT = process.env.PORT || 4000;
 dotenv.config();
 
 app.use(express.json()); // to parse the incoming requests with JSON payloads from res.body
+app.use(cookieParser());
 
 app.use("/api/auth", authRoutes);
+app.use("/api/message", messageRoutes);
+app.use("/api/users", userRoutes);
 
 // app.get("/", (req, res) => {
 //   // root route http://localhost:8000/
